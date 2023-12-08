319c05b9b7c9289870b7780d54981cdbb624313b
olena lukhta <olenaorashkevych@gmail.com>
2023-12-08 11:28:46 +0100

    connected my react app to backendless app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Backendless from 'backendless';
 import './App.css';
-import { Router, Routes, BrowserRouter} from 'react-router-dom';
+import {  Route, Routes} from 'react-router-dom';
+import { useState } from 'react';
+
 
 function App() {
+  Backendless.serverURL = "https://eu-api.backendless.com"
+  Backendless.initApp( process.env.REACT_APP_ID, process.env.REACT_APP_JS_API_KEY );
+  
   return (
     <div className="App">
-      
+      <Routes>
+        <Route> </Route>
+        <Route> </Route>
+        <Route> </Route>
+        <Route> </Route>
+      </Routes>
     </div>
   );
 }
