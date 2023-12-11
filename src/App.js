b331e2cf4ec7e59f143a9a7ab23ade5c6205a54b
olena lukhta <olenaorashkevych@gmail.com>
2023-12-11 14:06:30 +0100

    created a route to error page,to send a user if he is not loged in

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ import NewClient from './NewClient';
 import AllClients from './AllClients';
 import Addorseedatabase from './Addorseedatabase';
 import Loginpage from './Loginpage';
+import Errorpage from './Errorpage';
+
 
 
 function App() {
+  const [logedin, setloged] = useState(false);
   
-
+  Backendless.serverURL = "https://eu-api.backendless.com"
+  Backendless.initApp( process.env.REACT_APP_ID, process.env.REACT_APP_JS_API_KEY );
+  
   return (
    
  
     <div className="App">
-      {/* <Loginpage /> */}
       <Routes>
-        <Route path="/allclients" element={<AllClients/>}> </Route>
-        <Route path="/newclient" element={<NewClient/>}> </Route>
+        <Route path="/allclients" element={<AllClients logedin={logedin}setloged={setloged}/>}> </Route>
+        <Route path="/newclient" element={<NewClient logedin={logedin} setloged={setloged}/>}> </Route>
         <Route path="/addorseeclients" element={<Addorseedatabase/>}></Route>
-        <Route  path="/" element={<Loginpage/>}></Route>
+        <Route  path="/" element={<Loginpage logedin={logedin} setloged={setloged}/>}></Route>
+        <Route  path="/errorpage" element={<Errorpage/>}></Route>
+
       </Routes>
     </div>
   );
