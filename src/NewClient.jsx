f164043d44feea971dec55a743c30ff814f904d7
olena lukhta <olenaorashkevych@gmail.com>
2023-12-11 14:00:28 +0100

    created the daysyUI design for a new client input change, changed the text of it

--- src/NewClient.jsx
+++ src/NewClient.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 
-function NewClient() {
+function NewClient(logedin) {
 
   return (
     <div><div className="hero min-h-screen bg-base-200">
     <div className="hero-content flex-col lg:flex-row-reverse">
       <div className="text-center lg:text-left">
-        <h1 className="text-5xl font-bold">Login now!</h1>
-        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <h1 className="text-5xl font-bold">Create a new client now!</h1>
+        <p className="py-6">Please fill all the data</p>
       </div>
       <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <form className="card-body">
