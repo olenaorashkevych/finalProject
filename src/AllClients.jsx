17c00ea392f2428fc9ef2c78dcb11570a3cd6615
olena lukhta <olenaorashkevych@gmail.com>
2023-12-11 14:04:46 +0100

    created a condition so only loged in users can see the database

--- src/AllClients.jsx
+++ src/AllClients.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Backendless from "backendless";
+function AllClients({logedin,setloged}) {
 
-function AllClients() {
+    const navigate=useNavigate()
+    function logout() {
+    Backendless.UserService.logout()
+    .then((res) => console.log(res))
+    .catch((erro) => console.log(erro));
+   setloged(false);
+      }
+
+useEffect(()=>{
+    // what y need to do
+if (!logedin) {
+   navigate('/erropage') 
+}
+},[]
+)
+
   return (
-    <div>AllClients
-    <button className="btn btn-primary">Login</button>
-    <button className="btn btn-primary">Login</button>
+    <div>
+        
+    
+<button type="click" onClick={logout}> Log out</button>
+
+
+
     </div>
   )
 }
