ed40c633e79d474c4a4b2d3b3b7b595717466fc8
olena lukhta <olenaorashkevych@gmail.com>
2023-12-11 13:58:24 +0100

    created error page

---
+++ src/Errorpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+function Errorpage() {
+  return (
+    <div>Email or password is wrong</div>
+  )
+}
+
+export default Errorpage
