4bcf14e34905bd793d9341f2646d03f2bdfab18a
olena lukhta <olenaorashkevych@gmail.com>
2023-12-12 09:48:31 +0100

    Added more fields about client information, and a picture for this page

--- src/NewClient.jsx
+++ src/NewClient.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 
-function NewClient(logedin) {
+function NewClient({logedin,setloged}) {
 
   return (
     <div><div className="hero min-h-screen bg-base-200">
     <div className="hero-content flex-col lg:flex-row-reverse">
       <div className="text-center lg:text-left">
         <h1 className="text-5xl font-bold">Create a new client now!</h1>
-        <p className="py-6">Please fill all the data</p>
+        <div class="avatar">
+  <div class="w-48 rounded">
+    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+   
+  </div>
+</div>
       </div>
       <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <form className="card-body">
@@ -31,11 +36,29 @@ function NewClient(logedin) {
     <option>Choose client category</option>
     <option>Designer</option>
     <option>Construction Supervisor</option>
-    <option>Construction Rirm</option>
+    <option>Construction Firm</option>
     <option>Manufacturer</option>
     <option>Retail</option>
 
   </select>
+  <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+    <option>Source of Information</option>
+    <option>Company Website</option>
+    <option>Showroom Exterior</option>
+    <option>Company Google Profile</option>
+    <option>Friends</option>
+    <option>Online Digest</option>
+
+
+  </select>
+  <div className="form-control">
+            <label className="label">
+              <span className="label-text">Basic discount, %</span>
+            </label>
+
+            <input type="text" placeholder="10" className="input input-bordered" required />
+           
+          </div>
   <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
   </div>
@@ -46,7 +69,9 @@ function NewClient(logedin) {
         </form>
       </div>
     </div>
-  </div></div>
+  </div>
+  
+  </div>
   )
 }
 
