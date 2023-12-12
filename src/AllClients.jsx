26f09ff7ee597369ffccce52dd51d033f17d0580
olena lukhta <olenaorashkevych@gmail.com>
2023-12-12 12:18:20 +0100

    retrieved the all clients data from backend, put them in a table

--- src/AllClients.jsx
+++ src/AllClients.jsx
@@ -1,30 +1,87 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Backendless from "backendless";
-function AllClients({logedin,setloged}) {
 
-    const navigate=useNavigate()
-    function logout() {
-    Backendless.UserService.logout()
-    .then((res) => console.log(res))
-    .catch((erro) => console.log(erro));
-   setloged(false);
+
+
+function AllClients({logedin,setloged}) {
+const [Clients, setClients] = useState('')
+const navigate=useNavigate()
+
+function logout() {
+Backendless.UserService.logout()
+.then((res) => console.log(res))
+.catch((erro) => console.log(erro));
+setloged(false);
       }
+function seeallclients() {
+Backendless.Data.of( "Allclients" ).find()
+.then(  response => {console.log(response)
+setClients(response);}
+
+) 
+.catch( function( error )
+ {
+}); 
+}
+ 
+
 
 useEffect(()=>{
-    // what y need to do
 if (!logedin) {
+
    navigate('/erropage') 
+}
+else {
+    seeallclients()
 }
 },[]
 )
 
   return (
     <div>
-           
-<button type="click" onClick={logout}> Log out</button>
+    {Clients && Clients.map
+    (Client=>{
+      return(<div>
+        <table>
+        <tr>
+            <th>Client Name</th>
+            <th>Client Email</th>
+            <th>Source of Information</th>
+            <th>Client Category</th>
+            <th>Client Phone Number</th>
+            <th>Basic Discount</th>
+            <th>Date of Creation</th>
 
-</div>
+        </tr>
+        <tr>
+        <td>{Client.ClientName}</td>
+        <td>{Client.Email}</td>
+        <td>{Client.Source}</td>
+        <td>{Client.Category}</td>
+        <td>{Client.Phone_number}</td>
+        <td>{Client.Discount}</td>
+        <td>{Client.created}</td>
+
+        
+
+
+
+        </tr>
+        
+        </table>
+      
+      </div>
+      
+
+    )
+
+  }
+
+  )}
+    
+    
+    </div>
   )
 }
 
