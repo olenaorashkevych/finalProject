96ab5e2ffce8ea5ec7303ea0d0110040879b10c1
olena lukhta <olenaorashkevych@gmail.com>
2023-12-13 14:30:39 +0100

    centered the buttons

--- src/Addorseedatabase.jsx
+++ src/Addorseedatabase.jsx
@@ -3,9 +3,9 @@ import { Link, Route, Routes } from 'react-router-dom';
 
 function Addorseedatabase() {
   return (
-    <div>
-    <Link to="/allclients" className="btn btn-primary" >See all clients</Link>
-    <Link to="/newclient"className="btn btn-primary">Create a new client</Link>
+    <div className='twonavigationlinks'>
+    <Link to="/allclients" className="btn btn-primary linK" >See all clients</Link>
+    <Link to="/newclient"className="btn btn-primary linK">Create a new client</Link>
 
 
 
