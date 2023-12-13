14da49861a7a5f46456190efd5c264fba6644a08
olena lukhta <olenaorashkevych@gmail.com>
2023-12-13 13:54:24 +0100

    changed text in login page paragraph

--- src/Loginpage.jsx
+++ src/Loginpage.jsx
@@ -59,9 +59,7 @@ function Loginpage({logedin,setloged}) {
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Login now!</h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Please fill in all the fields
             </p>
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
