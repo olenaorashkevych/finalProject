f8771fe05d516ca9af62d1ea8e9005871d7626c3
olena lukhta <olenaorashkevych@gmail.com>
2023-12-14 09:25:53 +0100

    created a component to see all the sales

---
+++ src/Allsales.jsx
@@ -0,0 +1,12 @@
+
+import React, { useEffect, useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Backendless from "backendless";
+
+function Allsales() {
+  return (
+    <div>Allsales</div>
+  )
+}
+
+export default Allsales
