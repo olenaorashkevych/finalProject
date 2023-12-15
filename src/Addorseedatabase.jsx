9e57f8f2455ac0cc39f33a5a089c1e754945a8c6
olena lukhta <olenaorashkevych@gmail.com>
2023-12-15 13:31:10 +0100

    created a button(link to create a new sale)

--- src/Addorseedatabase.jsx
+++ src/Addorseedatabase.jsx
@@ -6,6 +6,7 @@ function Addorseedatabase() {
     <div className='twonavigationlinks'>
     <Link to="/allclients" className="btn btn-primary linK" >See all clients</Link>
     <Link to="/newclient"className="btn btn-primary linK">Create a new client</Link>
+    <Link to="/newsale"className="btn btn-primary linK">Create a new sale</Link>
     <Link to="/allsales"className="btn btn-primary linK">See sales records</Link>
     <Link to="/report"className="btn btn-primary linK">See the Report</Link>
 
