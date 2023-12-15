c79bf8371b651b6475690619bd2960dd8b943274
olena lukhta <olenaorashkevych@gmail.com>
2023-12-15 08:56:33 +0100

    added a link to see a report

--- src/Addorseedatabase.jsx
+++ src/Addorseedatabase.jsx
@@ -7,6 +7,7 @@ function Addorseedatabase() {
     <Link to="/allclients" className="btn btn-primary linK" >See all clients</Link>
     <Link to="/newclient"className="btn btn-primary linK">Create a new client</Link>
     <Link to="/allsales"className="btn btn-primary linK">See sales records</Link>
+    <Link to="/report"className="btn btn-primary linK">See the Report</Link>
 
 
 
