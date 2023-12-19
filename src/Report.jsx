020f20267f3cbdd14959a10a1d930e290ab4b6f0
olena lukhta <olenaorashkevych@gmail.com>
2023-12-19 10:21:43 +0100

    also converted to dateToString

--- src/Report.jsx
+++ src/Report.jsx
@@ -1,31 +1,52 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Backendless from "backendless";
 
 function Report({allsales, setallsales,logedin, setloged}) {
-    const [categoryfilter,setcategoryfilter]=useState()
+    // const [categoryfilter,setcategoryfilter]=useState()
+    // const [retailsales, setretailsales]=useState()
+  //  SUNDAY11111 // delete all the statuses, try do set only allsales as in all sales filters. if not, create all reports in all sales pge
+    
     const navigate=useNavigate()
-    function logout() {
-        Backendless.UserService.logout()
-        .then((res) => {
-        setloged(false)
-       navigate ("/errorpage")
-      }
-        )
-        .catch((erro) => console.log(erro));
-        
-              }
-    function categoryFilterdes () {
+    function seeallsales() {
+      Backendless.Data.of("Salesrecords")
+        .find({relations:["Client"]
+        })
+        .then((response) => {
+          console.log(response);
+          setallsales(response);
+        })
+        .catch(function (error) {});
+    }
+    useEffect(()=>{
+      seeallsales()
+      },[]
+      )
+      function categoryFilter () {
 
-        setcategoryfilter(sale => allsales.filter(sale => sale.Client.Category != "Retail"))
-        
+        setallsales(i => allsales.filter(sale => sale.Client.Category != "Retail"))
+      }
+      useEffect(()=>{
+          if (!logedin) {
+      
+            navigate('/') 
+         }
+         else {
+          seeallsales()
+          }
+        },[]
+        )
+      function categoryFilterretail () {
+  
+  
+        setallsales(i => allsales.filter(sale => sale.Client.Category == "Retail"))
       }
       
-  return (
-    <div className='overflow-x-auto'>
-    <button onClick={categoryFilterdes}>All B2B Sales</button>
+  return (<div className="flexiblecenter">
+    <div className='overflow-x-auto'> 
+    
   <table className="table table-xs ">
   <thead>
      <tr>
@@ -33,22 +54,23 @@ function Report({allsales, setallsales,logedin, setloged}) {
               <th>Client</th>
               <th>Sale date</th>
               <th>Summ</th>
-              <th>Source</th>
+              <th>Category</th>
               
       </tr>
   </thead>   
   <tbody>
 
-    {categoryfilter? categoryfilter.map(sale => <tr><td>{sale.number}</td><td>{sale.Client.ClientName}</td><td>  {(new Date(sale.salesdate)).getDay()}/{(new Date(sale.salesdate)).getMonth()}/{(new Date(sale.salesdate)).getFullYear()}</td>{sale.Summ} <td>{sale.Client.Source}</td></tr>) : allsales && allsales.map 
+    {allsales? allsales.map(sale => <tr><td>{sale.number}</td><td>{sale.Client.ClientName}</td><td>  {( new Date(sale.salesdate).toDateString())} </td>
+      {sale.Summ} <td>{sale.Client.Category}</td></tr>) : allsales && allsales.map 
           (sale => (<tr>
         <td>{sale.number}</td>
           <td>{sale.Client.ClientName}</td>
-          <td>{(new Date(sale.salesdate)).getFullYear() }/{(new Date(sale.salesdate)).getMonth()}/{(new Date(sale.salesdate)).getDay()}</td>
+          <td>{( new Date(sale.salesdate).toDateString())}</td>
           <td>{sale.Summ}</td>
-          <td>{sale.Client.Source}</td>
+          <td>{sale.Client.Category}</td>
           </tr>))}
     
-      {/* {} */}
+      
   
   </tbody>
       
@@ -64,11 +86,13 @@ function Report({allsales, setallsales,logedin, setloged}) {
       </tfoot>
       </table>
    
-  <button  onClick={logout} className="btn btn-primary linK">Log out</button>
-  {/* <button onClick={filternotactive} className="btn btn-primary linK"> Remind me to call these clients</button> */}
-  
+  {/* <button  onClick={logout} className="btn btn-primary linK">Log out</button> */}
+  <button className="btn btn-primary linK pushend" onClick={categoryFilter}>See business clients orders</button>
+  <button className="btn btn-primary linK pushstart" onClick={categoryFilterretail}>See retail clients order</button>
+  {/* <button className="btn btn-primary linK" onClick={categoryFilterretail}>See retail clients order</button> */}
       
      
+      </div>
       </div>
     )
   }
