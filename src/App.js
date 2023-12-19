cab5b71c6b495cd85d5d0013048799398cebea64
olena lukhta <olenaorashkevych@gmail.com>
2023-12-19 10:23:19 +0100

    added the route to a new component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Errorpage from './Errorpage';
 import Allsales from './Allsales';
 import Report from './Report';
 import Newsale from './Newsale';
+import Calls from './Calls';
+
 
 
 
@@ -21,26 +23,26 @@ function App() {
   Backendless.initApp( process.env.REACT_APP_ID, process.env.REACT_APP_JS_API_KEY );
   
 
-  function seeallsales() {
-    Backendless.Data.of("Salesrecords")
-      .find({relations:["Client"]
-      })
-      .then((response) => {
-        console.log(response);
-        setallsales(response);
-      })
-      .catch(function (error) {});
-  }
-  useEffect(()=>{
-    seeallsales()
-    },[]
-    )
+  // function seeallsales() {
+  //   Backendless.Data.of("Salesrecords")
+  //     .find({relations:["Client"]
+  //     })
+  //     .then((response) => {
+  //       console.log(response);
+  //       setallsales(response);
+  //     })
+  //     .catch(function (error) {});
+  // }
+  // useEffect(()=>{
+  //   seeallsales()
+  //   },[]
+  //   )
   return (
    
  
     <div className="App">
       <Routes>
-        <Route path='/allsales' element={<Allsales/> } />
+        <Route path='/allsales' element={<Allsales logedin={logedin}setloged={setloged} allsales={allsales}setallsales={setallsales}/> } />
         <Route path="/allclients" element={<AllClients logedin={logedin}setloged={setloged}/>}> </Route>
         <Route path="/newclient" element={<NewClient logedin={logedin} setloged={setloged}/>}> </Route>
         <Route path="/addorseeclients" element={<Addorseedatabase/>}></Route>
@@ -48,6 +50,7 @@ function App() {
         <Route  path="/errorpage" element={<Errorpage/>}></Route>
         <Route  path="/report" element={<Report allsales={allsales} setallsales={setallsales}logedin={logedin}setloged={setloged}/>}></Route>
         <Route  path="/newsale" element={<Newsale allsales={allsales} setallsales={setallsales}logedin={logedin}setloged={setloged}/>}></Route>
+        <Route  path="/calls" element={<Calls allsales={allsales} setallsales={setallsales}logedin={logedin}setloged={setloged}/>}></Route>
       </Routes>
     </div>
   );
